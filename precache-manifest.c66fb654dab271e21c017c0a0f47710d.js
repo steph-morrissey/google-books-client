68c19022565b9556318b9a0f450044c7a43c19f6
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-09-07 21:56:19 +0100

    Updates

---
+++ precache-manifest.c66fb654dab271e21c017c0a0f47710d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93898ea7402dc126ef67ed9aa39716e7",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3650820a710cd6b06875",
+    "url": "/google-books-client/static/js/main.9818dc51.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
